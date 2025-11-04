e9f1fbe1268d22d7caa84be494f2bf8b66ceceb7
Sweth shaw <116083207+swethshaw@users.noreply.github.com>
2025-11-04 15:49:28 +0530

    Fisrt Commit

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+require("./conn/conn");
+const cors = require("cors");
+const UserAPI = require("./routes/user");
+const TaskAPI = require("./routes/task");
+app.use(cors());
+app.use(express.json());
+app.use("/api/v1", UserAPI);
+app.use("/api/v2", TaskAPI);
+
+const PORT = process.env.PORT || 1000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running at http://192.168.1.11:${PORT}`);
+});
---
+++ frontend/src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import store from "./store";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </StrictMode>
+);
+
+// Optionally measure performance
+reportWebVitals();
